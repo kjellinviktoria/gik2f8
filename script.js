e17e6557c3476534294d0ef2b3450b7b4c97f098
Viktoria Kjellin <h21vikkj@du.se>
2022-12-12 20:32:45 +0000

    refactoring

--- script.js
+++ script.js
@@ -13,36 +13,15 @@ const bookList = [
   }
 ];
 
-/*const searchInput = document.children[0].children[1].children[1].children[1];*/
-const searchField = document.getElementById('searchField');
-
-/* keydown, keyup */
-searchField.addEventListener("keyup", handleKeyPress);
-
-function handleKeyPress(e) {
-  /* Ta emot/läsa av värdet i inputfältet.
-  Skicka värdet till searchBooks
-  SearchBooks returnerar en filtrerad lista.
-  Filtrerade listan skickas till renderlist */
-  searchBooks(e.target.value);
-}
-
-function searchBooks(searchTerm) {
-  /* Loopa igenom bookList
-  För varje varv i loopen, ta det aktuella elementet (boken)
-  Jämför titeln med söktermen
-  Om söktermen finns någonstans i titeln, lägg till elementet i ny lista (filteredList) 
-  Returnerar filteredList eller anropar renderBookList */
-
-  const filteredList = [];
-  for (let i = 0; i < bookList.length; i++) {
-    const title = bookList[i].title.toLowerCase();
-    if(title.indexOf(searchTerm.toLowerCase()) >= 0) {
-      filteredList.push(bookList[i]);
-    }
-  }
-  renderBookList(filteredList);
-}
+searchField.addEventListener("keyup", (e) =>
+  renderBookList(
+    bookList.filter(({title, author}) => {
+      const searchTerm = e.target.value.toLowerCase();
+      return title.toLowerCase().indexOf(searchTerm) >= 0 || 
+      author.toLowerCase().indexOf(searchTerm)>= 0;
+    })
+  )
+);
 
 function renderBookList(bookList) {
   /* Element i HTML-listan visas/döljs beroende på listans innehåll. */
