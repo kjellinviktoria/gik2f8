5168659dca5906280587f2abe848a9549f75ab10
Viktoria Kjellin <h21vikkj@du.se>
2022-12-12 18:52:00 +0000

    Lyssnar efter input.

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+const bookList = [
+  {
+    id: 1,
+    author: 'Charles Dickens',
+    title: 'Oliver Twist'
+  },
+  {
+    id: 2,
+    author: 'William Shakespear',
+    title: 'Hamlet'
+  }
+];
+
+/*const searchInput = document.children[0].children[1].children[1].children[1];*/
+const searchField = document.getElementById('searchField');
+console.log(searchField);
+
+/* keydown, keyup */
+searchField.addEventListener("keyup", handleKeyPress);
+
+function handleKeyPress(e) {
+  /* Ta emot/läsa av värdet i inputfältet.
+  Skicka värdet till searchBooks
+  SearchBooks returnerar en filtrerad lista.
+  Filtrerade listan skickas till renderlist */
+  searchBooks(e.target.value);
+}
+
+function searchBooks(searchTerm) {
+  /* Loopa igenom bookList
+  För varje varv i loopen, ta det aktuella elementet (boken)
+  Jämför titeln med söktermen
+  Om söktermen finns någonstans i titeln, lägg till elementet i ny lista (filteredList) 
+  Returnerar filteredList eller anropar renderBookList */
+
+  const filteredList = [];
+  for (let i = 0; i < bookList.length; i++) {
+    const title = bookList[i].title.toLowerCase();
+    if(title.indexOf(searchTerm.toLowerCase()) >= 0) {
+      filteredList.push(bookList[i]);
+    }
+  }
+  renderBookList(filteredList);
+}
+
+function renderBookList(list) {
+  /* Element i HTML-listan visas/döljs beroende på listans innehåll. */
+  console.log(list);
+}
