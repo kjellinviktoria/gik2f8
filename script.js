cf93dcedaafc63b02f23204e6ce8270a66f14fb2
Viktoria Kjellin <h21vikkj@du.se>
2022-12-12 21:18:21 +0000

    api-kommunikation

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 'use strict';
 
-const bookList = [
-  {
-    id: 1,
-    author: 'Charles Dickens',
-    title: 'Oliver Twist'
-  },
-  {
-    id: 2,
-    author: 'William Shakespear',
-    title: 'Hamlet'
-  }
-];
+let bookList = [];
 
-searchField.addEventListener("keyup", (e) =>
+window.addEventListener("load", () => {
+  getAll().then((apiBooks) => (bookList = apiBooks));
+});
+
+searchField.addEventListener('keyup', (e) => searchBooks(e.target.value));
+
+/*searchField.addEventListener("keyup", (e) =>
   renderBookList(
     bookList.filter(({title, author}) => {
       const searchTerm = e.target.value.toLowerCase();
@@ -21,19 +16,24 @@ searchField.addEventListener("keyup", (e) =>
       author.toLowerCase().indexOf(searchTerm)>= 0;
     })
   )
-);
+); */
+
+async function searchBooks(searchTerm) {
+  renderBookList(
+    bookList.filter(
+      ({ title, author }) =>
+        title.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0 ||
+        author.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0
+    )
+  );
+}
 
 function renderBookList(bookList) {
-  /* Element i HTML-listan visas/döljs beroende på listans innehåll. */
-
   const existingElement = document.querySelector('.book-list');
   console.log(existingElement);
 
   const root = document.getElementById('root');
-  if(existingElement) {
-    root.removeChild(existingElement);
-  }
-  if(bookList.length > 0) { 
-    root.insertAdjacentHTML('beforeend', BookList(bookList)); 
-  }
+
+  existingElement && root.removeChild(existingElement);
+  bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList)); 
 }
